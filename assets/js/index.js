0371a6f7efb50083cd29ac2b342747c105f2887d
huangjs <huangjs@ai-english.com.cn>
2021-09-28 21:15:20 +0800

    更新

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,4 @@
-console.log("感谢您的浏览，期待能够加入贵公司！我的博客：http://0b11111110.com/");
+console.log("感谢您的浏览，期待能够加入贵公司！我的博客：http://0b11111110xx.com/");
+const ageI = document.getElementById('age')
+const age = new Date().getFullYear() - 1995 + 1
+ageI.innerHTML = age
